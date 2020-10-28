24228c6c0bb6ccb5ab8d98fd5ca815378be9a7e4
Andrew ND <andrew.nc.dm@gmail.com>
2020-10-28 12:20:34 +0200

    Create Base state with scriptable objects

---
+++ Playing With Sprites/Assets/AdventureGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    Text textComponent;
+
+    [SerializeField]
+    State statingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = statingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Playing With Sprites/Assets/State.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField]
+    [TextArea(14, 10)]
+    string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+}
